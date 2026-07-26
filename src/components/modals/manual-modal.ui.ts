2b2ffdb98148eb19f2858cbdbd470a7740ddec3e
Ankit Pandey <ankit1pandey11@gmail.com>
2026-07-27 02:15:16 +0530

    updated the version number and also iphone pwa stuff for responsive ness

--- src/components/modals/manual-modal.ui.ts
+++ src/components/modals/manual-modal.ui.ts
@@ -7,7 +7,7 @@ export const manualModal = `
           <svg width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="var(--docs-accent)" stroke-width="3">
             <path d="M4 19.5A2.5 2.5 0 0 1 6.5 17H20"/><path d="M6.5 2H20v20H6.5A2.5 2.5 0 0 1 4 19.5v-15A2.5 2.5 0 0 1 6.5 2z"/>
           </svg>
-          <span class="docs-version">v4.2.0</span>
+          <span class="docs-version">v4.3.0</span>
         </div>
 
         <div class="docs-search-shell">
